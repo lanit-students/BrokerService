5973d2941d6774e08742cabb7b135bcda66490b2
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-03-08 14:51:04 +0300

    Add Controller Method

---
+++ TinkoffIntegrationLibrary/TBClass.cs
@@ -0,0 +1,18 @@
+﻿using DTO;
+using InterfaceBrokerIntegrationLibrary;
+using System;
+using System.Collections.Generic;
+
+namespace TinkoffIntegrationLibrary
+{
+    public class TBClass : IBroker
+    {
+        public IList<Course> A()
+        {
+            return new List<Course>()
+            {
+                new Course(){Name = "slkdjgbf"},
+            };
+        }
+    }
+}
